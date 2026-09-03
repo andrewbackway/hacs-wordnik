17838a0c8547bcbe9bf29dce977f6cb1ef46faf3
andrewbackway <31836236+andrewbackway@users.noreply.github.com>
2026-09-03 20:05:47 +1000

    Release v0.1.11

--- custom_components/wordnik/const.py
+++ custom_components/wordnik/const.py
@@ -5,7 +5,7 @@ from __future__ import annotations
 from typing import Final
 
 DOMAIN: Final = "wordnik"
-VERSION: Final = "0.1.10"
+VERSION: Final = "0.1.11"
 PLATFORMS: Final = ["sensor"]
 
 # Config / options keys
